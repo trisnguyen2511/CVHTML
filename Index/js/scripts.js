75df857b99407796c75636f2ed9fdb28c2ed6060
trisnguyen2511 <trisnguyen25112000@gmail.com>
2021-08-04 22:14:54 +0700

    intro

--- Index/js/scripts.js
+++ Index/js/scripts.js
@@ -360,3 +360,25 @@
 	});
 
 })(jQuery);
+
+
+function ageCalculator() {
+    var dob = new Date("2000-11-25");
+    {
+    
+    //calculate month difference from current date in time
+    var month_diff = Date.now() - dob.getTime();
+    
+    //convert the calculated difference in date format
+    var age_dt = new Date(month_diff); 
+    
+    //extract year from date    
+    var year = age_dt.getUTCFullYear();
+    
+    //now calculate the age of the user
+    var age = Math.abs(year - 1970);
+    
+    //display the calculated age
+    return document.getElementById("result").innerHTML =  age;
+    }
+}
